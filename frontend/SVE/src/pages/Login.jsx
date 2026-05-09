084704f8ba338519f60c0bd6c5a71cde2dce9bb8
RCosta210606 <rodrigofcosta7@gmail.com>
2026-05-09 03:28:43 +0100

    Finalização da página de Login com dinãmismo

--- frontend/SVE/src/pages/Login.jsx
+++ frontend/SVE/src/pages/Login.jsx
@@ -5,74 +5,62 @@ import "../css/Login.css";
 function Login() {
     return (
         <section className="login-page">
-            <div className="container-fluid h-100">
-                <div className="row h-100">
+            <div className="login-card">
+                <div className="login-left">
+                    <img
+                        src={logoImage}
+                        alt="Game DSA Awards"
+                        className="login-logo"
+                    />
 
-                    <div className="col-sm-6 text-black login-left">
-                        <div className="logo-container">
-                            <img
-                                src={logoImage}
-                                alt="Game DSA Awards"
-                                className="login-logo"
+                    <form className="login-form">
+                        <h1>Log in</h1>
+
+                        <div className="mb-4">
+                            <label className="form-label" htmlFor="email">
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="form-control form-control-lg"
                             />
                         </div>
 
-                        <div className="login-form-container">
-                            <form className="login-form">
-                                <h1 className="fw-normal mb-4">Log in</h1>
-
-                                <div className="form-outline mb-4">
-                                    <input
-                                        type="email"
-                                        id="emailinput"
-                                        className="form-control form-control-lg"
-                                    />
-                                    <label className="form-label" htmlFor="form2Example18">
-                                        Email address
-                                    </label>
-                                </div>
-
-                                <div className="form-outline mb-4">
-                                    <input
-                                        type="password"
-                                        id="passwordinput"
-                                        className="form-control form-control-lg"
-                                    />
-                                    <label className="form-label" htmlFor="form2Example28">
-                                        Password
-                                    </label>
-                                </div>
-
-                                <div className="pt-1 mb-4">
-                                    <button className="btn btn-info btn-lg" type="button">
-                                        Login
-                                    </button>
-                                </div>
-
-                                <p className="small mb-5">
-                                    <a className="text-muted" href="#!">
-                                        Forgot password?
-                                    </a>
-                                </p>
-
-                                <p>
-                                    Don't have an account?{" "}
-                                    <a href="#!" className="link-info">
-                                        Register here
-                                    </a>
-                                </p>
-                            </form>
+                        <div className="mb-4">
+                            <label className="form-label" htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="form-control form-control-lg"
+                            />
                         </div>
-                    </div>
 
-                    <div className="col-sm-6 px-0 d-none d-sm-block">
-                        <img
-                            src={loginImage}
-                            alt="Login"
-                            className="login-image"
-                        />
-                    </div>
+                        <button className="btn btn-info btn-lg login-button" type="button">
+                            Login
+                        </button>
 
+                        <a className="forgot-link" href="#!">
+                            Forgot password?
+                        </a>
+
+                        <p className="register-text">
+                            Don't have an account?{" "}
+                            <a href="#!" className="link-info">
+                                Register here
+                            </a>
+                        </p>
+                    </form>
+                </div>
+
+                <div className="login-right">
+                    <img
+                        src={loginImage}
+                        alt="Game Awards"
+                        className="login-image"
+                    />
                 </div>
             </div>
         </section>
