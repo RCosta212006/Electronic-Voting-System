b417de29528980a0cac2ed3bffa9e632ee73f9c9
RCosta210606 <rodrigofcosta7@gmail.com>
2026-05-31 17:36:09 +0100

    Criação do componente de Carregar os Cartões

---
+++ frontend/src/components/LoadingCards.jsx
@@ -0,0 +1,16 @@
+import "../css/LoadingCards.css";
+
+function LoadingCards() {
+    return (
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    );
+}
+
+export default LoadingCards;
